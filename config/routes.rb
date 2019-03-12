3cfa942e2a331ecc333a7be440995b8b67e90acb
PhongPhamdt <phongpham.hedspi@gmail.com>
2019-03-12 11:12:48 +0700

    refs RORTUTS #chapter_5 fixed coding convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-    get "static_pages/help"
-    get "static_pages/contact"
-    get "static_pages/about"
+    get "/help", to: "static_pages#help"
+    get "/contact", to: "static_pages#contact"
+    get "/about", to: "static_pages#about"
+    get "/signup", to: "user#new"
   end
 end
