2ad9151afb97e9f3c8307612b47e33cbd68e9da0
PhongPhamdt <phongpham.hedspi@gmail.com>
2019-04-04 11:35:57 +0700

    refs RORTUTS #chapter_14 deleted redundant files

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     return if logged_in?.nil?
     @micropost = current_user.microposts.build
-    @feed_items =
-      current_user.microposts.sort_by_created.paginate(page: params[:page])
+    @feed_items = Micropost.feed(current_user)
+                           .sort_by_created
+                           .paginate(page: params[:page],
+                            per_page: Settings.per_page)
   end
 
   def help; end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,13 +2,18 @@ class Micropost < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true
-
   validates :content, presence: true,
   length: {maximum: Settings.maximum_content}
-
   validate :picture_size
 
   scope :sort_by_created, ->{order created_at: :desc}
+
+  scope(:feed, lambda do |user|
+    where(select("followed_id")
+      .from("relationships")
+      .where("followed_id = ? ", user).include?(:user_id))
+      .or(where(user_id: user))
+  end)
 
   mount_uploader :picture, PictureUploader
 
